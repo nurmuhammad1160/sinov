0a32fcea2750e4fbbaa2781e1c28ec4793b66360
Nurmuhammad <oddiybola1160@gmail.com>
2023-11-07 15:07:50 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# def malumot():
+#     info = f"ism: {ism}, Falimiya: {familiya}, {h_yil}"
+#     return info
+    
+# malumot('Nurmuhammad', 'Samadov')
+
+def sonlar(*args, **kwargs):
+    print(args, kwargs)
+
+
+sonlar(1,2,3,4,4,5,son=20,son2=40)
+
